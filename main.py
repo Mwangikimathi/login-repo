ee5e1c6f6592da159d9d27b295dbeed6b42097a3
Mwangikimathi <kimmwangi46@gmail.com>
2021-03-14 13:48:19 +0300

    login commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask,redirect,request,render_template,url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template ('base.html')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
